fc8fc5adf0e7984a9f5b36650a88542e7e831bbf
moxa-rumin <romamuhino1@gmail.com>
2022-11-16 17:01:21 +0530

    moxa

--- js/header.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($){ //wait for the document to load
-    $('.trans-header').each(function(){ //loop through each element with the .dynamic-height class
-        $(this).css({
-            'height' : $(".header").height() + 'px' //adjust the css rule for margin-top to equal the element height - 10px and add the measurement unit "px" for valid CSS
-        });
-    });
-});
